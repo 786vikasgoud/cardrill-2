089c6164d897d6eed5109f1f41bcf431eaef8d4d
786vikasgoud <vikkydada421@gmail.com>
2023-11-17 22:26:15 +0530

    initialchanhe

--- problem1.js
+++ problem1.js
@@ -3,7 +3,6 @@ function problem1(inventery){
     let ans=inventery.filter(function(element){
         return element.id===33;
     })
-    console.log(ans["car_make"]);
     return ans;
 }
 module.exports=problem1;
