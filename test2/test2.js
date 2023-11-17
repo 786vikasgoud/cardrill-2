7bc21c20a799c73e0da23a528386ee170cb8d7d1
786vikasgoud <vikkydada421@gmail.com>
2023-11-17 22:33:49 +0530

    initialcommit

--- test2/test2.js
+++ test2/test2.js
@@ -1,4 +1,5 @@
 let problem2 = require("../problem2.js");
 let inventery = require("../inventery.js");
 const sol=problem2(inventery);
-console.log(sol);
+const sol2=sol[0];
+console.log(`Last car is ${sol2.car_make} model ${sol2.car_model}`);
