80ae037a39937d4563088f8ce87db50750edc24c
786vikasgoud <vikkydada421@gmail.com>
2023-11-17 22:15:24 +0530

    initialchange

--- test2/test1.js
+++ test2/test1.js
@@ -1,4 +1,5 @@
 let problem1 = require("../problem1.js");
 let inventery = require("../inventery.js");
 let sol1=problem1(inventery);
-console.log(`Car 33 is a ${sol1.car_year} ${sol1.car_make} ${sol1.car_model}`);
+const ans=sol1[0];
+console.log(`Car 33 is a ${ans.car_year} ${ans.car_make} ${ans.car_model}`);
