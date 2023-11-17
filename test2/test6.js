655101341970975580b13272bf16c04223bf13d5
786vikasgoud <vikkydada421@gmail.com>
2023-11-17 22:58:55 +0530

    initialcommir

--- test2/test6.js
+++ test2/test6.js
@@ -2,5 +2,5 @@ let problem6 = require("../problem6.js");
 let inventery = require("../inventery.js");
 
 const sol6=problem6(inventery);
-let bmwAudiCars= JSON.stringify(sov6);
+let bmwAudiCars= JSON.stringify(sol6);
 console.log(bmwAudiCars);
