3a10b5688a254544f9a2c3d38b62ea7a67903dd5
786vikasgoud <vikkydada421@gmail.com>
2023-11-17 22:24:15 +0530

    initial change

--- problem1.js
+++ problem1.js
@@ -3,6 +3,7 @@ function problem1(inventery){
     let ans=inventery.filter(function(element){
         return element.id===33;
     })
+    console.log(ans["car_make"]);
     return ans;
 }
 module.exports=problem1;
